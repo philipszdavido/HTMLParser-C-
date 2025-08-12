d9fef1db78acf5a1e24082869033c24998ded336
Chidume Nnamdi <kurtwanger40@gmail.com>
2025-08-12 11:21:04 +0100

    added Node type

---
+++ HTMLParser C++/htmlparser/Node.h
@@ -0,0 +1,29 @@
+//
+//  Node.h
+//  HTMLParser C++
+//
+//  Created by Chidume Nnamdi on 11/08/2025.
+//
+
+#ifndef Node_h
+#define Node_h
+
+using namespace std;
+
+enum class NodeType {
+    Text,
+    Element
+};
+
+class Node {
+    
+public:
+    Node(string name, NodeType type, const string textContent) : name(name), nodeType(type), textContent(textContent) {};
+    const string name;
+    NodeType nodeType;
+    const string textContent;
+    const vector<Node> children;
+    
+};
+
+#endif /* Node_h */
