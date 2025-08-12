23f4f8ddf243de4906647ada87e0b406717f59f5
Chidume Nnamdi <kurtwanger40@gmail.com>
2025-08-12 11:21:34 +0100

    added logic to parse tokens

--- HTMLParser C++/htmlparser/htmlparser.cpp
+++ HTMLParser C++/htmlparser/htmlparser.cpp
@@ -6,4 +6,81 @@
 //
 
 #include "htmlparser.hpp"
+#include "../tokenizer/Tokenizer.hpp"
+#include "../overloads/operators.h"
+#include <iostream>
 
+using namespace std;
+
+vector<Node> HTMLParser::parse() {
+    
+    Tokenizer tokenizer(html);
+    vector<Token> tokens = tokenizer.tokenize();
+    
+    vector<Node> nodes;
+    
+    for (int i = 0; i < tokens.size(); ++i) {
+        
+        const Token token = tokens[i];
+        
+        if(token.token == TokenType::Text) {
+            
+            nodes.push_back(Node("", NodeType::Text, token.name));
+            
+        } else if (token.token == TokenType::Element) {
+            
+            if(token.isVoid) {
+                
+                nodes.push_back(Node(token.name, NodeType::Element, ""));
+                
+            }
+            
+            // search if element has children
+            vector<Token> children = getTokenChildren(tokens, token.name, i + 1);
+
+            for (Token t : children) {
+                cout << t << endl;
+            }
+            
+            
+        }
+        
+    }
+    
+    return nodes;
+    
+}
+
+vector<Token> HTMLParser::getTokenChildren(vector<Token> tokens, const string& name, int index) {
+
+    int seen = 0;
+    vector<Token> children;
+    
+    for (int i = index; i < tokens.size(); ++i) {
+
+        const Token token = tokens[i];
+        
+        if(token.name == name) {
+            
+            if(token.start) {
+                seen++;
+            }
+            
+            if (token.end) {
+                seen--;
+            }
+            
+            if(seen == 0) {
+                break;
+            }
+            
+        }
+        
+        children.push_back(token);
+        
+        
+    }
+    
+    return children;
+    
+}
--- HTMLParser C++/htmlparser/htmlparser.hpp
+++ HTMLParser C++/htmlparser/htmlparser.hpp
@@ -10,15 +10,20 @@
 
 #include <stdio.h>
 #include <iostream>
+#include "../models/Token.hpp"
+#include "Node.h"
 
 using namespace std;
 
 class HTMLParser {
     
 public:
-    HTMLParser(const string& htmlString) {}
+    HTMLParser(const string& htmlString): html(htmlString) {};
+    vector<Node> parse();
+    vector<Token> getTokenChildren(vector<Token> tokens, const string& name, int index);
     
-    
+private:
+    const string& html;
     
 };
 
